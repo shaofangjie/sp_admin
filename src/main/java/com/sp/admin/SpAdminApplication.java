237e50841fe68cc0ed8a3fc09a3d151e82ee165a
shaofangjie <shaofangjie@gamil.com>
2021-09-05 23:41:00 +0800

    [ADD] 添加项目打包配置
    添加项目打包配置

--- src/main/java/com/sp/admin/SpAdminApplication.java
+++ src/main/java/com/sp/admin/SpAdminApplication.java
@@ -3,13 +3,21 @@ package com.sp.admin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.sp.admin.dao") //扫描的mapper
-public class SpAdminApplication {
+public class SpAdminApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpAdminApplication.class, args);
+    }
+
+    @Override//为了打包springboot项目
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
     }
 
 }
